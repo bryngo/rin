06ed05b779cfc9e9585d42731d5d6b63378533a3
Bryan Ngo <bryanngo97@gmail.com>
2019-08-20 18:44:07 -0700

    cleaned unused vars. default play volume to 50

--- commands/play.js
+++ commands/play.js
@@ -11,7 +11,10 @@ exports.run = async (client, message, args) => {
     // if the bot has permission to play music in the author's voice channel
     if(message.member.voice.channel && message.member.voice.channel.permissionsFor(client.user).has('SPEAK')) {
         const voiceConnection = await message.member.voice.channel.join();
-        voiceConnection.play(ytdl(`${args[0]}`, { quality: 'highestaudio' }));
+        const voiceDispatcher = voiceConnection.play(ytdl(`${args[0]}`, { quality: 'highestaudio' }));
+
+        // default to a volume of .5
+        voiceDispatcher.setVolume(50 / 100);
 
         message.channel.send(`--- ▶ Playing ▶ --- \n ${args[0]}`);
 
